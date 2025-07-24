82872892ec2a38385574fc48de8d3f5547f4ed31
tt4keru <tt4keru@gmail.com>
2025-07-25 07:41:13 +0900

    プロジェクト追加

---
+++ Assets/Scripts/InOutRefTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class InOutRefTest : MonoBehaviour {
+
+	void Start() {
+		int a = 1;
+		int b = 1;
+		int c = 1;
+
+		Debug.Log($"開始：a = {a}, c = {c}");
+
+		RefMethod(ref a);
+		OutMethod(out b);
+		InMethod(in c);
+
+		Debug.Log($"終了：a = {a}");
+		Debug.Log($"終了：b = {b}");
+		Debug.Log($"終了：c = {c}");
+	}
+
+	void RefMethod(ref int a) {
+		a += 9;	// 呼び出し元の値を書き換える
+		Debug.Log($"[RefMethod] a = {a}");
+	}
+
+	void OutMethod(out int b) {
+		b = 10;	// 初期化が必要
+		Debug.Log($"[OutMethod] b = {b}");
+	}
+
+	void InMethod(in int c) {
+		//c += 9; 読み取り専用のためエラー
+		Debug.Log($"[InMethod] c = {c}");
+	}
+}
